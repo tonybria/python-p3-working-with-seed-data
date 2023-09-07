418dcbcb45833934a8dbd695861d75353fafb13d
tony-brian <btoni3823@gmail.com>
2023-09-07 21:40:36 +0300

    completed assignment

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,12 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+
+    session.bulk_save_objects([botw, ffvii, mk8])
+    session.commit()
+
+    print("Seeded database with 3 games")
